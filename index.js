7d4c0ac7d247f9aa9a507a9c06385465f4f5ea21
Amruth Gudigar <57893983+AmruthGudigar111@users.noreply.github.com>
2025-07-03 22:18:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('ChatVault API'));
 app.listen(5000, () => console.log('Server running on port 5000'));
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+const cors = require('cors');
+app.use(cors({
+  origin: '*', // or set to your Vercel frontend URL for security
+}));
